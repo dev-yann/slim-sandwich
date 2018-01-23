75dc49c630cd0c6bb8f432d18181921a7df2ed6b
nico <bobelet.nicolas@gmail.com>
2018-01-23 17:12:03 +0100

    Tout est nouveau, ceci est mieux organisé

--- api_private/index.php
+++ api_private/index.php
@@ -6,9 +6,9 @@
  * Time: 15:24
  */
 
-require_once  __DIR__ . '/../src_private/vendor/autoload.php';
-$error = require_once __DIR__ . '/../src_private/error/error.php';
-$settings = require_once  __DIR__ . '/../src_private/conf/settings.php';
+require_once  __DIR__ . '/../src/vendor/autoload.php';
+$error = require_once __DIR__ . '/../src/error/error.php';
+$settings = require_once  __DIR__ . '/../src/conf/settings.php';
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
@@ -18,7 +18,7 @@ use lbs\control\Home;
 
 
 // initialisation connection
-$config = parse_ini_file('../src_private/conf/lbs.db.conf.ini');
+$config = parse_ini_file('../src/conf/lbs.db.conf.ini');
 $db = new Illuminate\Database\Capsule\Manager();
 $db->addConnection( $config );
 $db->setAsGlobal();
@@ -36,7 +36,7 @@ $app = new \Slim\App( new \Slim\Container($app_config));
 
 
 // require de toutes mes routes
-require_once __DIR__.'/../src_private/route/routes.php';
+require_once __DIR__.'/../src/route/routes_private.php';
 
 
 
